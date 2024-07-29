ed1a47774720b78d6b297b5c82fc4e3e0534043f
Sufiyaan Usmani <usmanisufiyaan@gmail.com>
2024-07-30 01:59:33 +0500

    Add routes for v1
    - `/api/v1` -> GET
    - `/api/v1/song/{song_id}` -> GET
    - `/api/v1/add` -> POST

--- Dockerfile
+++ Dockerfile
@@ -13,5 +13,7 @@ RUN pip install --no-cache-dir -r requirements.txt
 # Copy the rest of the application code into the container at /app
 COPY . .
 
+EXPOSE 8000
+
 # Command to run the FastAPI application using Uvicorn
 CMD ["uvicorn", "api.main:app", "--host", "0.0.0.0", "--port", "8000", "--reload"]
---
+++ api/v1/routes/routes.py
@@ -0,0 +1,14 @@
+from fastapi import APIRouter
+
+from api.v1.controllers.songs import add_song, get_song, root
+
+router = APIRouter()
+
+# /api/v1 -> GET
+router.get("/")(root)
+
+# /api/v1/song/{song_id} -> GET
+router.get("/song/{song_id}")(get_song)
+
+# /api/v1/add -> POST
+router.post("/add")(add_song)
